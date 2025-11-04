6325db71975cdcd69ba2f5a928d9f454f45a2f27
Rachitha-krishnamurthy <your_github_email@example.com>
2025-11-05 03:33:27 +0530

    Set Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/my-portfolio/", // <-- This tells Vite to use the correct base path for GitHub Pages
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
